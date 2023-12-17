f0f9eb45810602c29a961dd6f2c0d3964c0dbb3e
firikibay <153018366+firikibay@users.noreply.github.com>
2023-12-17 18:18:20 +0300

    Time: 3 ms (88.68%) | Memory: 7.2 MB (12.76%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    if (numRows <= 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    // Bellekte matris için yer ayırma
+    int** result = (int**)malloc(numRows * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
+
+    for (int i = 0; i < numRows; i++) {
+        result[i] = (int*)malloc((i + 1) * sizeof(int));
+        (*returnColumnSizes)[i] = i + 1;
+
+        // İlk ve son elemanlar 1
+        result[i][0] = result[i][i] = 1;
+
+        // Diğer elemanlar üstteki iki elemanın toplamı
+        for (int j = 1; j < i; j++) {
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+    }
+
+    *returnSize = numRows;
+    return result;
+}
