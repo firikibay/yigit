5fe89f5408f1caa8d4cd711cf7878deb9a2dd778
firikibay <153018366+firikibay@users.noreply.github.com>
2023-12-17 18:08:23 +0300

    Time: 14 ms (44.83%) | Memory: 8.4 MB (19.08%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize <= 1) {
+        return numsSize;
+    }
+
+    int k = 1;  // Benzersiz öğelerin sayısı
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
